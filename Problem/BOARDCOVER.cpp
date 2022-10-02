af31bab005a5bbed1d000a7231a72d5b1040ec9e
N図形⚓ <53482136+Nzukei@users.noreply.github.com>
2022-10-03 00:10:40 +0900

    Fix runtime error

--- Problem/BOARDCOVER.cpp
+++ Problem/BOARDCOVER.cpp
@@ -25,13 +25,14 @@ const int coverType[4][3][2] = {
 */
 bool set(vector<vector<int>>& board, int y, int x, int type, int delta) {
     bool ok = true;
-    for (int i = 0; i < 3; ++i) {
+    
+    for (int i = 0; i < 3; i++) {
         const int ny = y + coverType[type][i][0];
         const int nx = x + coverType[type][i][1];
         
         if (ny < 0 || ny >= board.size() || nx < 0 || nx >= board[0].size())
             ok = false;
-        else if ((board[ny][nx] += delta > 1))
+        else if ((board[ny][nx] += delta) > 1)
             ok = false;
     }
     return ok;
@@ -47,8 +48,8 @@ int cover(vector<vector<int>>& board) {
     // 아직 채우지 못한 칸 중 가장 윗줄 왼쪽에 있는 칸을 찾는다.
     int y = -1, x = -1 ;
     
-    for (int i = 0; i < board.size(); ++i) {
-        for (int j = 0; j < board[i].size(); ++j)
+    for (int i = 0; i < board.size(); i++) {
+        for (int j = 0; j < board[i].size(); j++)
             if (board[i][j] == 0) {
                 y = i;
                 x = j;
@@ -61,7 +62,7 @@ int cover(vector<vector<int>>& board) {
     if (y == -1) return 1;
     int ret = 0;
     
-    for (int type = 0; type < 4; ++type) {
+    for (int type = 0; type < 4; type++) {
         // 만약 board[y][x]를 type 형태로 덮을 수 있으면 재귀 호출한다.
         if (set(board, y, x, type, 1))
             ret += cover(board);
@@ -90,6 +91,7 @@ int main() {
                     board[i][j] = 1;
             }
         }
+        
         cout << cover(board) << '\n';
     }
 }
