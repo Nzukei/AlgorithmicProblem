1f1868d7c3547156daa335ed8c7343b45d37eabd
N図形 <53482136+Nzukei@users.noreply.github.com>
2022-10-19 23:41:56 +0900

    Deleted include code

--- Problem/BOARDCOVER.cpp
+++ Problem/BOARDCOVER.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <string>
 
 
 using namespace std;
@@ -10,10 +9,10 @@ using namespace std;
  주어진 칸을 덮을 수 있는 네 가지 방법
 */
 const int coverType[4][3][2] = {
-    { { 0, 0 }, { 1, 0 }, { 0, 1 } }, // (b)
-    { { 0, 0 }, { 0, 1 }, { 1, 1 } }, // (c)
-    { { 0, 0 }, { 1, 0 }, { 1, 1 } }, // (d)
-    { { 0, 0 }, { 1, 0 }, { 1, -1 } } // (e)
+        { { 0, 0 }, { 1, 0 }, { 0, 1 } }, // (b)
+        { { 0, 0 }, { 0, 1 }, { 1, 1 } }, // (c)
+        { { 0, 0 }, { 1, 0 }, { 1, 1 } }, // (d)
+        { { 0, 0 }, { 1, 0 }, { 1, -1 } } // (e)
 };
 
 
@@ -24,11 +23,11 @@ const int coverType[4][3][2] = {
 */
 bool set(vector<vector<int>>& board, int y, int x, int type, int delta) {
     bool ok = true;
-    
+
     for (int i = 0; i < 3; i++) {
         const int ny = y + coverType[type][i][0];
         const int nx = x + coverType[type][i][1];
-        
+
         if (ny < 0 || ny >= board.size() || nx < 0 || nx >= board[0].size())
             ok = false;
         else if ((board[ny][nx] += delta) > 1)
@@ -47,7 +46,7 @@ int cover(vector<vector<int>>& board) {
     // 아직 채우지 못한 칸 중 가장 윗줄 왼쪽에 있는 칸을 찾는다.
     int y = -1, x = -1 ;
     int ret = 0;
-    
+
     for (int i = 0; i < board.size(); i++) {
         for (int j = 0; j < board[i].size(); j++)
             if (board[i][j] == 0) {
@@ -55,12 +54,12 @@ int cover(vector<vector<int>>& board) {
                 x = j;
                 break;
             }
-            if (y != -1) break;
+        if (y != -1) break;
     }
-    
+
     // 기저 사례: 모든 칸을 채웠으면 1을 반환한다.
     if (y == -1) return 1;
-    
+
     for (int type = 0; type < 4; type++) {
         // 만약 board[y][x]를 type 형태로 덮을 수 있으면 재귀 호출한다.
         if (set(board, y, x, type, 1))
@@ -68,7 +67,7 @@ int cover(vector<vector<int>>& board) {
         // 덮었던 블록을 치운다.
         set(board, y, x, type, -1);
     }
- 
+
     return ret;
 }
 
@@ -78,7 +77,7 @@ int main() {
     int y, x;
 
     cin >> test;
-    
+
     for (int i = 0; i < test; i++) {
         cin >> y >> x;
         vector<vector<int>> board(y, vector<int>(x, 0));
@@ -90,7 +89,7 @@ int main() {
                     board[i][j] = 1;
             }
         }
-        
+
         cout << cover(board) << '\n';
     }
 }
